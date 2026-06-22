69953b6bd65f47e4cd62c504c869258421d0e729
Raul-Sabin Petrut <raul.petrut44@yahoo.com>
2026-06-22 15:44:46 +0300

    Reorganized the codebase and changed the implementation of PPM support
    Now has P3 and P6 support

---
+++ image_viewer_utils.c
@@ -0,0 +1,42 @@
+#include "image_viewer_utils.h"
+#include "netpbm_utils.h"
+
+#include <stdlib.h>
+
+uint8_t *read_file_bytes(FILE* fd, long *file_len) {
+
+    fseek(fd, 0, SEEK_END);
+    *file_len = ftell(fd);
+    if (*file_len < 1) {
+        *file_len = 0;
+        return NULL;
+    }
+
+    rewind(fd);
+
+    uint8_t *bytes = malloc(*file_len * sizeof(uint8_t));
+    if (!bytes) {
+        fputs("Could not allocate memory for bytes buffer.\n", stderr);
+        *file_len = 0;
+        return NULL;
+    }
+
+    const size_t bytes_read =  fread(bytes, 1, *file_len, fd);
+    if (bytes_read != (size_t)*file_len) {
+
+        free(bytes);
+        *file_len = 0;
+        return NULL;
+    }
+
+    return bytes;
+}
+
+img_file_type determine_file_type(const uint8_t *bytes, const size_t bytes_count) {
+
+    img_file_type type = detect_netpmb(bytes, bytes_count);
+    if (type != UNSUPPORTED)
+        return type;
+
+    return UNSUPPORTED;
+}
